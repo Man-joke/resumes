0dbc247fc4d55856bb2d900a141556c04d812cc2
Man-joke <akstns90@gmail.com>
2024-06-27 22:31:07 +0900

    profile 세팅 진행중

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,14 +1,36 @@
-import { Typography } from "antd";
+import { Col, Flex, Row, Space, Typography } from "antd";
 import React from "react";
-const { Title, Text } = Typography;
+const { Title } = Typography;
 const profile = () => {
   return (
-    <>
-      <div>
-        <Title level={3}>Profile</Title>
-        <Text>I'm Web Publisher</Text>
+    <section className="section">
+      <div className="section-top">
+        <Title level={3} className="title title-h3">
+          Profile
+        </Title>
+        <p className="text">I'm Web Publisher</p>
       </div>
-    </>
+
+      <Row>
+        <Col span={24}>
+          <Flex justify="space-between">
+            <Space>이미지</Space>
+            <Space>프로필</Space>
+          </Flex>
+          <Row>
+            <Col>
+                <img src="" alt="프로필" />
+                <ul>
+                  <li>이력사항</li>
+                  <li>이력사항</li>
+                  <li>이력사항</li>
+                  <li>이력사항</li>
+                </ul>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </section>
   );
 };
 
