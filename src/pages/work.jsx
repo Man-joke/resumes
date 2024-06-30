13a4ebec6926cb4fbc29e50169bdcf196c31e597
Man-joke <akstns90@gmail.com>
2024-07-01 00:05:17 +0900

    work 작업 중

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import {  Typography } from "antd";
+import React from "react";
+import Works from "../components/Works";
+const { Title } = Typography;
 const work = () => {
   return (
-    <div>work</div>
-  )
-}
+    <section className="section">
+      <div className="section-top">
+        <Title level={3} className="title title-h3">
+          PROFILE
+        </Title>
+        <p className="text">WORK EXPERIENCE</p>
+      </div>
+      <div className="works-wrap">
+        <Works />
+      </div>
+    </section>
+  );
+};
 
-export default work
+export default work;
